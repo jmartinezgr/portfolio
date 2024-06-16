0f3544055929a0a47f41b0526bb51eccb2f37ee0
Juan <josemargri3@gmail.com>
2024-06-16 14:42:06 -0500

    feat : Agregar funcionalidad de cambiar  el lenguaje a los botones
    Se crea una función handleOnClick  para comparar la ruta y re navegar, a la nueva ruta con el lenguaje cambiado

--- src/components/layout/HeaderNav.jsx
+++ src/components/layout/HeaderNav.jsx
@@ -1,49 +1,58 @@
-import { NavLink } from "react-router-dom"
+import { NavLink, useNavigate } from "react-router-dom"
 import { useLanguage } from "../../hooks/useLanguage"
 
 const HeaderNav = () => {
-    
-    const lang = useLanguage()
-    
+    const lang = useLanguage();
+    const navigate = useNavigate();
+
+    const handleOnClick = (l) => {
+        if (l !== lang) {
+            const currentPath = window.location.pathname;
+            console.log(currentPath)
+
+            const newPath = currentPath.replace(`/${lang}/`, `/${l}/`);
+            console.log(newPath)
+            navigate(newPath)
+        }
+    }
+
     return (
         <header>
             <div className="logo">
                 <span>J</span>
-                <h3> Juan Martinez FULLSTACK DEV</h3>
+                <h3>Juan Martinez FULLSTACK DEV</h3>
             </div>
-                <nav>
-                    <ul>
-                        <li>
-                            <NavLink to={`/${lang}/inicio`}>{lang === 'es' ? 'Inicio' : 'Home'}</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to={`/${lang}/portafolio`}>{lang === 'es' ? 'Portafolio' : 'Portfolio'}</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to={`/${lang}/servicios`}>{lang === 'es' ? 'Servicios' : 'Services'}</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to={`/${lang}/curriculum`}>{lang === 'es' ? 'Curriculum' : 'Resume'}</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to={`/${lang}/contacto`}>{lang === 'es' ? 'Contacto' : 'Contact'}</NavLink>
-                        </li>
-                        <li>
-                            <button>
-                                <img src="/español.png" alt="" />
-                            </button>
-                        </li>
-                        <li>
-                            <button>
-                                <img src="/english.png" alt="" />
-                            </button>
-                        </li>
-                    </ul>
-                </nav>
+            <nav>
+                <ul>
+                    <li>
+                        <NavLink to={`/${lang}/inicio`}>{lang === 'es' ? 'Inicio' : 'Home'}</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to={`/${lang}/portafolio`}>{lang === 'es' ? 'Portafolio' : 'Portfolio'}</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to={`/${lang}/servicios`}>{lang === 'es' ? 'Servicios' : 'Services'}</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to={`/${lang}/curriculum`}>{lang === 'es' ? 'Curriculum' : 'Resume'}</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to={`/${lang}/contacto`}>{lang === 'es' ? 'Contacto' : 'Contact'}</NavLink>
+                    </li>
+                    <li>
+                        <button onClick={() => handleOnClick('es')}>
+                            <img src="/español.png" alt="Español" />
+                        </button>
+                    </li>
+                    <li>
+                        <button onClick={() => handleOnClick('en')}>
+                            <img src="/english.png" alt="English" />
+                        </button>
+                    </li>
+                </ul>
+            </nav>
         </header>
     )
 }
-
-
 
 export default HeaderNav
