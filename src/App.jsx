a158b4a9b219da06068390c2c7b33847e028e223
Juan <josemargri3@gmail.com>
2024-06-17 16:16:29 -0500

    fix: Arreglar etiquetas css de color negro que no usaban las variables
    Existian etiquetas css que usaban el color default por lo que a la hora de modificar las variables de color estas no cambiaban

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
 import MisRutas from './routers/MisRutas'
 
 function App() {
+
+	if(true){
+		document.documentElement.style.setProperty('--background-color', '#222D89')
+		document.documentElement.style.setProperty('--text-pcolor', '#FFFFFF')
+		document.documentElement.style.setProperty('--text-scolor', '#ddd')
+		document.documentElement.style.setProperty('--primary-color', '#4183FF')
+	}
+
 	return (
 		<>
 			<MisRutas />
