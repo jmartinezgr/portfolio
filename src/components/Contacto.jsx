6013807c080bf6b11d55577b6b92ebcf6d001e99
Juan <josemargri3@gmail.com>
2024-06-17 22:19:36 -0500

    feat: Agregar funcionalidad de que la app contacto sirva en ambos idiomas

--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -1,18 +1,42 @@
+import { useLanguage } from '../hooks/useLanguage'
 import '../styles/Contacto.css'
 
+const translations = {
+    "es": {
+        "heading": "Contacto",
+        "placeholderNombre": "Nombre",
+        "placeholderApellido": "Apellido",
+        "placeholderEmail": "Email",
+        "placeholderMotivo": "Motivo de contacto",
+        "submitValue": "Enviar"
+    },
+    "en": {
+        "heading": "Contact",
+        "placeholderNombre": "First Name",
+        "placeholderApellido": "Last Name",
+        "placeholderEmail": "Email",
+        "placeholderMotivo": "Reason for contact",
+        "submitValue": "Submit"
+    }
+}
+
 const Contacto = () => {
+    const lang = useLanguage(); // Obtener el idioma actual
+
+    const translation = translations[lang];
+
     return (
         <div className="page">
-            <h1 className="heading">Contacto</h1>
+            <h1 className="heading">{translation.heading}</h1>
             <form action="mailto:juan.martinez.desarollador@gmail.com" className="contact">
-                <input type="text" placeholder="Nombre"/>
-                <input type="text" placeholder="Apellido"/>
-                <input type="text" placeholder="Email"/>
-                <textarea placeholder="Motivo de contacto"/>                
-                <input type="submit" value="Enviar"/>
+                <input type="text" placeholder={translation.placeholderNombre}/>
+                <input type="text" placeholder={translation.placeholderApellido}/>
+                <input type="text" placeholder={translation.placeholderEmail}/>
+                <textarea placeholder={translation.placeholderMotivo}/>                
+                <input type="submit" value={translation.submitValue}/>
             </form>
         </div>
     )
 }
 
-export default Contacto
+export default Contacto;
