320891f5e6f9a9a9fc09f5e4084051badb91d750
Juan <josemargri3@gmail.com>
2024-06-26 12:28:23 -0500

    feat : Actualizando urls y descripciones de proyectos

--- src/assets/data/trabajos.js
+++ src/assets/data/trabajos.js
@@ -2,22 +2,21 @@ export const trabajos = {
     "es": [
         {
             'id': '1',
-            'nombre': 'jmartinezgr Portafolio',
+            'nombre': 'jmartinezgr Portafolio 1.0',
             'url': 'https://jmartinezgr.github.io/',
             'tecnologias': ['HTML', 'CSS', 'JavaScript'],
-            'categorias': 'Desarrollo Web',
+            'categorias': ['Desarrollo Web','Trabajo de Pago'],
             'desc': 'Portafolio personal para mostrar mis trabajos y habilidades en el desarrollo web.',
-            'github': 'https://sbetav.com/'            
+            'github': 'https://github.com/jmartinezgr/jmartinezgr.github.io'            
         },
         {
             'id': '2',
-            'nombre': 'Centro Medico',
-            'url': 'https://centromedico.com',
+            'nombre': 'Gestion Clinica',
+            'url': 'https://gestionclinicaapp.pythonanywhere.com/',
             'tecnologias': ['HTML', 'CSS', 'Bootstrap', 'Django'],
-            'categorias': 'Desarrollo Web',
-            'desc': 'Página web para un centro médico que permite a los usuarios agendar citas y ver los servicios que ofrece.',
-            'github': 'https://sbetav.com/'            
-            
+            'categorias': ['Desarrollo Web', 'Trabajo de Pago'],
+            'desc': "El proyecto GestionClinica, encargado para la gestión de una pequeña clínica, implica la transferencia de información entre las partes interesadas. Cuenta con APIs que cumplen con los criterios de madurez de Richardson, un front-end desarrollado con Bootstrap 5, y una lógica completamente desarrollada utilizando Django, incluyendo decoradores personalizados y una estructura de interacción de datos robusta. El código fuente en GitHub incluye claves y usuarios para probar todos los roles dentro de las aplicaciones. Este proyecto fue desarrollado para un estudiante como parte de su trabajo académico y fue un esfuerzo remunerado.",
+            'github': 'https://github.com/jmartinezgr/GestionClinica '             
         },
         {
             'id': '3',
@@ -60,39 +59,39 @@ export const trabajos = {
     "en": [
         {
             'id': '1',
-            'nombre': 'jmartinezgr Portfolio',
+            'nombre': 'jmartinezgr Portfolio 1.0',
             'url': 'https://jmartinezgr.github.io/',
             'tecnologias': ['HTML', 'CSS', 'JavaScript'],
-            'categorias': 'Web Development',
+            'categorias': ['Web Development', 'Paid Work'],
             'desc': 'Personal portfolio to showcase my work and skills in web development.',
-            'github': 'https://sbetav.com/'
+            'github': 'https://github.com/jmartinezgr/jmartinezgr.github.io'
         },
         {
             'id': '2',
-            'nombre': 'Medical Center',
-            'url': 'https://centromedico.com',
-            'tecnologias': ['HTML', 'CSS', 'Bootstrap', 'Django'],
-            'categorias': 'Web Development',
-            'desc': 'Website for a medical center that allows users to schedule appointments and view the services offered.',
-            'github': 'https://sbetav.com/'
+            'nombre': 'Clinic Management',
+            'url': 'https://gestionclinicaapp.pythonanywhere.com/',
+            'tecnologias': ['HTML', 'CSS', 'JavaScript', 'Django', 'Bootstrap', 'APIREST'],
+            'categorias': ['Web Development', 'Paid Work'],
+            'desc': "The GestionClinica project, commissioned for a small clinic's management, involves information transfer among stakeholders. It features APIs adhering to Richardson maturity criteria, a Bootstrap 5 front-end, and a fully developed logic using Django, including custom decorators and a robust data interaction structure. The source code on GitHub includes keys and user accounts to test all roles within the apps. This project was developed for a student as part of their academic work and was a paid endeavor.",
+            'github': 'https://github.com/jmartinezgr/GestionClinica '
         },
         {
             'id': '3',
             'nombre': 'Statistics WebPage',
-            'url': 'https://estadisticasWebPage.com',
-            'tecnologias': ['HTML', 'CSS', 'JavaScript'],
+            'url': 'https://easyestadistica.pythonanywhere.com/',
+            'tecnologias': ['HTML', 'CSS', 'JavaScript', 'Python', 'Django'],
             'categorias': 'Web Development',
             'desc': 'Website that displays statistics of a website, such as visits, time on site, user location, etc.',
-            'github': 'https://sbetav.com/'
+            'github': 'https://github.com/jmartinezgr/EstadisticasWebPage'
         },
         {
             'id': '4',
-            'nombre': 'Trello Clone',
-            'url': 'https://trelloClone.com',
-            'tecnologias': ['HTML', 'React', 'NodeJs', 'MongoDB'],
+            'nombre': 'Local Cinema Project',
+            'url': 'https://cinelocal.pythonanywhere.com/peliculas/',
+            'tecnologias': ['HTML', 'CSS', 'JavaScript', 'Python', 'Django'],
             'categorias': 'Web Development',
-            'desc': 'Clone of the Trello application, allowing users to create boards and organize tasks.',
-            'github': 'https://sbetav.com/'
+            'desc': 'Application to manage the movie listings of a small cinema.',
+            'github': 'https://github.com/jmartinezgr/ProyectoCine'
         },
         {
             'id': '5',
